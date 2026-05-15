6e1f2612f96057b6d8acc7a144a7d139313502e6
Reed VSC <reed.grant@my.bdsc.school.nz>
2026-05-15 13:30:15 +1200

    started with version 1

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-def test():
-    test = "Hello from VSC"
-print(f'{test}')
-test()
+menu = [
+    "butter chicken pie",
+    "steak and cheese pie",
+    "mince and cheese pie",
+    "aqua cna",
+    "chicken sub",
+    "juicy",
+    "afghan cookie",
+    "pork steam bun"
+]
+
+print("\nWelcome to the school canteen!\n")
+for item in menu:
+    print(f"{item.title()}")
+
+order = input("\nWhat would you like to order:").lower()
+
+if order in menu:
+    print(f"You ordered a {order}, thank you have a nice day")
+else:
+    print("we do not have that item")
