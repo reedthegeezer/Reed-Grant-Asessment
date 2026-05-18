54fdd32f451826e7d0679169e6081ca0bf136af6
Reed VSC <reed.grant@my.bdsc.school.nz>
2026-05-18 14:10:30 +1200

    changing file name

--- version1.py
+++ version1.py
@@ -7,15 +7,15 @@ menu = [
     "juicy",
     "afghan cookie",
     "pork steam bun"
-]
+] #menu with some items
 
 print("\nWelcome to the school canteen!\n")
 for item in menu:
-    print(f"{item.title()}")
+    print(f"{item.title()}") #printing menu items
 
-order = input("\nWhat would you like to order:").lower()
+order = input("\nWhat would you like to order:").lower() #ordering what the user wants
 
 if order in menu:
     print(f"You ordered a {order}, thank you have a nice day")
 else:
-    print("we do not have that item")
+    print("we do not have that item") #checks if it has item ordered, if not prints it doesn't have it
