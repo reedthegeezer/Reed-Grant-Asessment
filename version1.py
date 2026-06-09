24452b8dad00e71f42de5da9d9ac5b0cf17c27e1
Reed VSC <reed.grant@my.bdsc.school.nz>
2026-06-10 09:29:29 +1200

    added two functions, display_menu and take_order, to organize code and change code easier later on

--- version1.py
+++ version1.py
@@ -1,21 +1,25 @@
+def display_menu(menu):
+    print("\nWelcome to the school canteen!\n")
+    for item in menu:
+        print(f"{item.title()}") #printing menu items
+
+def take_order(menu):
+    order = input("\nWhat would you like to order:").lower() #ordering what the user wants
+    if order in menu:
+        print(f"You ordered a {order}, thank you have a nice day")
+    else:
+        print("we do not have that item") #checks if it has item ordered, if not prints it doesn't have it
+
 menu = [
     "butter chicken pie",
     "steak and cheese pie",
     "mince and cheese pie",
-    "aqua cna",
+    "aqua can",
     "chicken sub",
     "juicy",
     "afghan cookie",
     "pork steam bun"
 ] #menu with some items
 
-print("\nWelcome to the school canteen!\n")
-for item in menu:
-    print(f"{item.title()}") #printing menu items
-
-order = input("\nWhat would you like to order:").lower() #ordering what the user wants
-
-if order in menu:
-    print(f"You ordered a {order}, thank you have a nice day")
-else:
-    print("we do not have that item") #checks if it has item ordered, if not prints it doesn't have it
+display_menu(menu)
+take_order(menu)
